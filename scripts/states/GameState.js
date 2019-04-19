be16bc3f707c565713bfecb6b6fa9801bbd61488
Anthony Jackman <energeticpixels@gmail.com>
2019-04-19 17:09:50 -0500

    adds enemy movement and bottom kill

--- scripts/states/GameState.js
+++ scripts/states/GameState.js
@@ -41,6 +41,8 @@ SpaceHipster.GameState = {
 
         var enemy = new SpaceHipster.Enemy(this.game, 100, 100, 'greenEnemy', 10, []);
         this.game.add.existing(enemy);
+        enemy.body.velocity.x = 100;
+        enemy.body.velocity.y = 50;
 
     },
 

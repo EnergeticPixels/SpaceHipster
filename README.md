23308cb313d174f34d80a7e9ff788994f07d259d
Anthony Jackman <energeticpixels@gmail.com>
2019-04-19 11:51:46 -0500

    basic project setup, adds moving background

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Space Hipster Game #
 
 This is a very, very simple game that demonstrates basic concepts of phaser.io. This course uses the 2.6.2 version of the game engine. This version is the last stable version from the game engine's original developers Photonstorm. This game is from the Udemy course "The Complete Mobile Game Development Course with Phaser". This game is modeled after my favorite teenager game "Space Invaders". This game demonstrates Intel's XDK framework for mobile development, game prefabs, particle explosion, multiple game levels and moving backgrounds (simulated starfield).
+
+First game created with namespace development concept.
---
+++ scripts/states/GameState.js
@@ -0,0 +1,38 @@
+var SpaceHipster = SpaceHipster || {};
+
+SpaceHipster.GameState = {
+    
+    //init game
+    init: function() {
+        this.scale.scaleMode - Phaser.ScaleManager.SHOW_ALL;
+
+        this.game.physics.startSystem(Phaser.Physics.ARCADE);
+
+        this.PLAYER_SPEED = 200;
+        this.BULLET_SPEED = -1000;
+
+    },
+
+    // load the gaem assets
+    preload: function() {
+        this.load.image('space', '../../images/space.png');
+        this.load.image('player', '../../images/player.png');
+        this.load.image('bullet', '../../images/enemyParticle.png');
+        this.load.spritesheet('yellowEnemy', '../../images/yellow_enemy.png', 50, 46, 3, 1, 1);
+        this.load.spritesheet('redEnemy', '../../images/red_enemy.png', 50, 46, 3, 1, 1);
+        this.load.spritesheet('greenEnemy', '../../images/green_enemy.png', 50, 46, 3, 1, 1)
+    },
+
+    // execute some stuff
+    create: function() {
+        this.background = this.add.tileSprite(0, 0, this.game.world.width, this.game.world.height, 'space');
+        this.background.autoScroll(0, 30);
+
+    },
+
+    // dynamically update
+    update: function() {
+
+    }
+
+}

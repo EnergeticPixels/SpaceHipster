cd24cc614423a7721e8dad3abe9e7efa4ef34e01
Anthony Jackman <energeticpixels@gmail.com>
2019-04-19 12:02:26 -0500

    adds player and touchscreen player controls

--- scripts/states/GameState.js
+++ scripts/states/GameState.js
@@ -20,7 +20,8 @@ SpaceHipster.GameState = {
         this.load.image('bullet', '../../images/enemyParticle.png');
         this.load.spritesheet('yellowEnemy', '../../images/yellow_enemy.png', 50, 46, 3, 1, 1);
         this.load.spritesheet('redEnemy', '../../images/red_enemy.png', 50, 46, 3, 1, 1);
-        this.load.spritesheet('greenEnemy', '../../images/green_enemy.png', 50, 46, 3, 1, 1)
+        this.load.spritesheet('greenEnemy', '../../images/green_enemy.png', 50, 46, 3, 1, 1);
+
     },
 
     // execute some stuff
@@ -28,11 +29,25 @@ SpaceHipster.GameState = {
         this.background = this.add.tileSprite(0, 0, this.game.world.width, this.game.world.height, 'space');
         this.background.autoScroll(0, 30);
 
+        // player
+        this.player = this.add.sprite(this.game.world.centerX, this.game.world.height - 50, 'player');
+        this.player.anchor.setTo(0.5);
+        this.game.physics.arcade.enable(this.player);
+        this.player.body.collideWorldBounds = true;
+
     },
 
     // dynamically update
     update: function() {
+        this.player.body.velocity.x = 0;
 
+        if(this.game.input.activePointer.isDown) {
+            var targetX = this.game.input.activePointer.position.x;
+            
+            var direction = targetX >= this.game.world.centerX ? 1 : -1;
+
+            this.player.body.velocity.x = direction * this.PLAYER_SPEED;
+        }
     }
 
 }
